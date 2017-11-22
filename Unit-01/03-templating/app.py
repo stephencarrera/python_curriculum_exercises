229af6a0dbe6d670ce5bedf48816ad526db5b43c
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-21 22:41:31 -0800

    complete part 2

--- Unit-01/03-templating/app.py
+++ Unit-01/03-templating/app.py
@@ -9,6 +9,25 @@ def say_name_and_age(name, age):
 	name = name.title()
 	return render_template('person.html', name=name, age=age)
 	
+# Part 2
+@app.route('/calculate')
+def calc():
+	return render_template('calc.html')
+
+@app.route('/math')
+def math():
+	calculation = request.args.get('calculation')
+	num1 = int(request.args.get('num1'))
+	num2 = int(request.args.get('num2'))
+
+	if calculation == 'add':
+		return "The sum is: " + str(num1 + num2)
+	elif calculation == 'subtract':
+		return "The difference is: " + str(num1 - num2)
+	elif calculation == 'multiply':
+		return "The product is: " + str(num1 * num2)
+	elif calculation == 'divide':
+		return "The quotient is: " + str(num1 / num2)
 
 if __name__ == "__main__":
 	app.run(debug=True)
