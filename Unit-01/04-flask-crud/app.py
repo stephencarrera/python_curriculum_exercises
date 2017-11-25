9beaf16baa8f0ff5c472dc8e8ae98ce280b9278e
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-24 21:13:58 -0800

    remove title block

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -48,7 +48,7 @@ def edit(id):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html', title='Page Not Found'), 404
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
