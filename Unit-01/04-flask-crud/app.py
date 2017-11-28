de564731ac02bda4b3008e445b0b5652e1f3bb24
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-27 17:54:05 -0800

    complete flask-crud with tests passing

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,45 +1,41 @@
-from flask import Flask, request, redirect, url_for, render_template, abort
+from flask import Flask, request, redirect, url_for, render_template
 from flask_modus import Modus
 from snack import Snack
 
-snacks = [
-	Snack('Pecans', 'Nuts')
-	]
+snack_list = []
 
 app = Flask(__name__)
 modus = Modus(app)
 
 def find_snack(id):
-	return [snack for snack in snacks if snack.id == id][0]
+    return next(snack for snack in snack_list if snack.id == id)
 
 @app.route('/')
 def root():
-	return redirect(url_for('index'))
+    return redirect(url_for('snacks'))
 
-@app.route('/snacks', methods=["GET", "POST"])
+@app.route('/snacks', methods = ["GET", "POST"])
 def index():
 	if request.method == "POST":
-		new_snack = Snack(request.form.get("name"), request.form.get("kind"))
-		snacks.append(new_snack)
+		snack_list.append(Snack(request.form["name"],request.form["kind"]))
 		return redirect(url_for('index'))
-	return render_template('index.html', snacks=snacks)
+	return render_template('index.html', snack_list=snack_list)
 
 @app.route('/snacks/new')
 def new():
-	return render_template('new.html')
+    return render_template('new.html')
 
 @app.route('/snacks/<int:id>', methods=["GET", "PATCH", "DELETE"])
 def show(id):
 	found_snack = find_snack(id)
 	if request.method == b"PATCH":
-		found_snack.name = request.form['name']
-		found_snack.kind = request.form['kind']
-		return redirect(url_for('index'))
+		found_snack.name = request.form["name"]
+		found_snack.kind = request.form["kind"]
+		return redirect(url_for('index'))				
 	if request.method == b"DELETE":
-		snacks.remove(found_snack)
+		snack_list.remove(found_snack)
 		return redirect(url_for('index'))
 	return render_template('show.html', snack=found_snack)
-
 
 @app.route('/snacks/<int:id>/edit')
 def edit(id):
@@ -52,4 +48,29 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True, port=3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Unit-01/04-flask-crud/snack.py
+++ Unit-01/04-flask-crud/snack.py
@@ -2,13 +2,13 @@
 
 class Snack():
 
-	count = 1
+	id = 1
 
 	def __init__(self, name, kind):
 		self.name = name
 		self.kind = kind
-		self.id = Snack.count
-		Snack.count += 1
+		self.id = Snack.id
+		Snack.id += 1
 
 	def __repr__(self):
 		return f"Snack #{self.id}; Name: {self.name}; Kind: {self.kind}"
